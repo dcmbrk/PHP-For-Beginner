696a3a51929011b442c650cfb0e7c31012fe8644
Forty1 <ldtung41@gmail.com>
2025-09-03 19:20:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
-//van de hien tai la href cua nav dang hardcore -> CSS hien thi cung 1 kieu tren nhieu trang
-//Can sua lai de CSS hien thi tuy vao url cua trang -> Kiem tra url hien tai roi hien thi dung navbar cua muc
-//Nhieu trang co cung mot kieu hien thi -> dung function de tai su dung
-//Function kiem tra url chung chung nen cho vao functions.php
-//Kiem tra bang Global Variable -> Dung ham DD -> ham DD chung chung nen cung cho vao funcions.php
-
-//PHP se load url
-//  Neu ten mien co ten.php co trong local folder thi se load file do
-//  Khong thi tu dong load file index.php nhung van giu ten mien
-
 require "functions.php";
+require "Database.php";
+// require "routes.php";
 
-$heading = "Home";
 
-require "views/index.view.php";
+$config = require "config.php";
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query,[
+  ':id' => $id
+]
+)->fetchAll();
+
+dd($posts);
+
