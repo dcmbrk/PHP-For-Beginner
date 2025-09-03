1fc8d553876f42a0d12109da09a9ab4a33ded6ff
Forty1 <ldtung41@gmail.com>
2025-09-03 20:49:33 +0700

    Add files via upload

---
+++ controllers/note.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = require "config.php";
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+  ':id' => $_GET['id']
+])->fetch();
+
+if(! $note){
+  abort();
+}
+
+if( $note['user_id'] !== $currentUserId){
+  abort(Response::FORBIDDEN);
+}
+
+require "views/note.view.php";
