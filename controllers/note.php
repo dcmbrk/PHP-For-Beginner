0ced3d976adf29e8ebb9764ea0c72687044d61f8
Forty1 <ldtung41@gmail.com>
2025-09-06 09:48:10 +0700

    Add files via upload

---
+++ controllers/note.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = require "config.php";
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+  ':id' => $_GET['id']
+])->findOrFail();
+
+authorize(($currentUserId === $note['user_id']));
+
+require "views/note.view.php";
---
+++ controllers/notes.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = require "config.php";
+
+$heading = "My Notes";
+
+$db = new Database($config['database']);
+$notes = $db->query('SELECT * FROM notes')->get();
+
+require "views/notes.view.php";
