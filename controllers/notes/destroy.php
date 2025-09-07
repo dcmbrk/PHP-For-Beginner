3d7aa7236612e3b0749bf603fc5055b74bcd9b65
Forty1 <ldtung41@gmail.com>
2025-09-07 20:17:09 +0700

    Add files via upload

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,21 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path("config.php");
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+':id' => $_GET['id']
+])->findOrFail();
+
+authorize(($currentUserId === $note['user_id']));
+
+$db->query('DELETE FROM notes WHERE id = :id', [
+  ':id' => $_POST['id']
+]);
+
+header('location: /notes');
+exit();
