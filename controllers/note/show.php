371fd18898d8b3e4bb8e720a275b5bd5448d940c
Forty1 <ldtung41@gmail.com>
2025-09-07 07:31:09 +0700

    Add files via upload

---
+++ controllers/note/show.php
@@ -0,0 +1,19 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path("config.php");
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+  ':id' => $_GET['id']
+])->findOrFail();
+
+authorize(($currentUserId === $note['user_id']));
+
+view("note/show.view.php",[
+  'heading' => 'Note',
+  'note' => $note
+]);
